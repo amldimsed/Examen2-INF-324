f663f2a7634109775c867973e4dc2783ce147223
amldimsed <ads,dark@gmail.com>
2022-12-05 00:28:01 -0400

    p3

---
+++ pregunta3/ImagenPix/ImagenPix/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿
+namespace ImagenPix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.punto_X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.punto_Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pix_R = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pix_G = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pix_B = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Abrir Imagen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Leer Pixel(10,10)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(46, 129);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(46, 158);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(46, 187);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "R";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "G";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "B";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(46, 257);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(46, 285);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 260);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "X";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 290);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Y";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(46, 332);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 40);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "cambiar1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(46, 378);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(84, 35);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "cambiar2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(46, 419);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(84, 37);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "cambiar3";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(171, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(752, 649);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.punto_X,
+            this.punto_Y,
+            this.pix_R,
+            this.pix_G,
+            this.pix_B});
+            this.dataGridView1.Location = new System.Drawing.Point(866, 187);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(406, 207);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // punto_X
+            // 
+            this.punto_X.HeaderText = "punto_X";
+            this.punto_X.MinimumWidth = 6;
+            this.punto_X.Name = "punto_X";
+            this.punto_X.Width = 80;
+            // 
+            // punto_Y
+            // 
+            this.punto_Y.HeaderText = "punto_Y";
+            this.punto_Y.MinimumWidth = 6;
+            this.punto_Y.Name = "punto_Y";
+            this.punto_Y.Width = 80;
+            // 
+            // pix_R
+            // 
+            this.pix_R.HeaderText = "pix_R";
+            this.pix_R.MinimumWidth = 6;
+            this.pix_R.Name = "pix_R";
+            this.pix_R.Width = 80;
+            // 
+            // pix_G
+            // 
+            this.pix_G.HeaderText = "pix_G";
+            this.pix_G.MinimumWidth = 6;
+            this.pix_G.Name = "pix_G";
+            this.pix_G.Width = 80;
+            // 
+            // pix_B
+            // 
+            this.pix_B.HeaderText = "pix_B";
+            this.pix_B.MinimumWidth = 6;
+            this.pix_B.Name = "pix_B";
+            this.pix_B.Width = 80;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(29, 522);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(117, 50);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "COLOREAR";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1284, 704);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn punto_X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn punto_Y;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pix_R;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pix_G;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pix_B;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ pregunta3/ImagenPix/ImagenPix/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImagenPix
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        Color c = new Color();
+        Color cleido = new Color();
+
+        int x = 0, y = 0;
+        int mR = 0, mG = 0, mB = 0;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+       
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+            textBox4.Text = x.ToString();
+            textBox5.Text = y.ToString();
+            
+            mR = 0; mG = 0; mB = 0;
+            for (int i = x; i < x + 10; i++)
+                for (int j = y; j < y + 10; j++)
+                {
+                    c = bmp.GetPixel(i, j);
+                    mR = mR + c.R;
+                    mG = mG + c.G;
+                    mB = mB + c.B;
+                }
+            mR = mR / 100;
+            mG = mG / 100;
+            mB = mB / 100;
+            textBox1.Text = mR.ToString();
+            textBox2.Text = mG.ToString();
+            textBox3.Text = mB.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            c = bmp.GetPixel(10, 10);
+            textBox1.Text = c.R.ToString();
+            textBox2.Text = c.G.ToString();
+            textBox3.Text = c.B.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            cleido = bmp.GetPixel(x, y);
+            GrabarDatos();
+            Bitmap bmp3 = new Bitmap(bmp.Width, bmp.Height);
+            //Color c = new Color();
+            for (int i = 0; i < bmp3.Width; i++)
+                for (int j = 0; j < bmp3.Height; j++)
+                {
+                    c = bmp.GetPixel(i, j);
+                    if (((cleido.R - 10 <= c.R) && (c.R <= cleido.R + 10)) &&
+                        ((cleido.G - 10 <= c.G) && (c.G <= cleido.G + 10)) &&
+                        ((cleido.B - 10 <= c.B) && (c.B <= cleido.B + 10)))
+                    {
+                        bmp3.SetPixel(i, j, Color.Yellow);
+                    }
+                    else
+                    {
+                        bmp3.SetPixel(i, j, Color.FromArgb(c.R, c.G, c.B));
+                    }
+
+                }
+            pictureBox1.Image = bmp3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            cleido = bmp.GetPixel(x, y);
+            GrabarDatos();
+            Bitmap bmp3 = new Bitmap(bmp.Width, bmp.Height);
+            //Color c = new Color();
+            for (int i = 0; i < bmp3.Width; i++)
+                for (int j = 0; j < bmp3.Height; j++)
+                {
+                    c = bmp.GetPixel(i, j);
+                    if (((cleido.R - 10 <= c.R) && (c.R <= cleido.R + 10)) &&
+                        ((cleido.G - 10 <= c.G) && (c.G <= cleido.G + 10)) &&
+                        ((cleido.B - 10 <= c.B) && (c.B <= cleido.B + 10)))
+                    {
+                        bmp3.SetPixel(i, j, Color.Blue);
+                    }
+                    else
+                    {
+                        bmp3.SetPixel(i, j, Color.FromArgb(c.R, c.G, c.B));
+                    }
+
+                }
+            pictureBox1.Image = bmp3;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            cleido = bmp.GetPixel(x, y);
+            GrabarDatos();
+            Bitmap bmp3 = new Bitmap(bmp.Width, bmp.Height);
+            //Color c = new Color();
+            for (int i = 0; i < bmp3.Width; i++)
+                for (int j = 0; j < bmp3.Height; j++)
+                {
+                    c = bmp.GetPixel(i, j);
+                    if (((cleido.R - 10 <= c.R) && (c.R <= cleido.R + 10)) &&
+                        ((cleido.G - 10 <= c.G) && (c.G <= cleido.G + 10)) &&
+                        ((cleido.B - 10 <= c.B) && (c.B <= cleido.B + 10)))
+                    {
+                        bmp3.SetPixel(i, j, Color.Cyan);
+                    }
+                    else
+                    {
+                        bmp3.SetPixel(i, j, Color.FromArgb(c.R, c.G, c.B));
+                    }
+
+                }
+            pictureBox1.Image = bmp3;
+        }
+
+        private void GrabarDatos()
+        {
+            c = bmp.GetPixel(10, 10);
+            StreamWriter archivo = new StreamWriter("pintura.txt", true);
+            archivo.WriteLine(x.ToString());
+            archivo.WriteLine(y.ToString());
+
+            archivo.WriteLine(c.R.ToString());
+            archivo.WriteLine(c.G.ToString());
+            archivo.WriteLine(c.B.ToString());
+
+            archivo.Close();
+        }
+
+      
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists("pintura.txt"))
+            {
+                StreamWriter archivo = new StreamWriter("pintura.txt");
+                archivo.Close();
+
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //**********IMPRIMIR EN CELDA-GRID LOS DATOS X,Y,R,G,B**********
+            StreamReader archivo = new StreamReader("pintura.txt");
+            while (!archivo.EndOfStream)
+            {
+                string xx = archivo.ReadLine();
+                string yy = archivo.ReadLine();
+
+                string PR = archivo.ReadLine();
+                string PG = archivo.ReadLine();
+                string PB = archivo.ReadLine();
+
+                dataGridView1.Rows.Add(xx, yy, PR, PG, PB);
+
+            }
+            archivo.Close();
+
+            //-----------PARA EL PRIMERA FILA 1------------------
+            int punto_x1 = 0, punto_y1 = 0;
+
+            punto_x1 = Convert.ToInt32(dataGridView1.Rows[0].Cells[0].Value);
+            punto_y1 = Convert.ToInt32(dataGridView1.Rows[0].Cells[1].Value);
+
+            cleido = bmp.GetPixel(punto_x1, punto_y1);
+            Bitmap bmp_x1_y1 = new Bitmap(pictureBox1.Image);
+            Bitmap bmpxy1 = new Bitmap(bmp_x1_y1.Width, bmp_x1_y1.Height);
+            //Color c = new Color();
+            for (int i = 0; i < bmpxy1.Width; i++)
+                for (int j = 0; j < bmpxy1.Height; j++)
+                {
+                    c = bmp_x1_y1.GetPixel(i, j);
+                    if (((cleido.R - 10 <= c.R) && (c.R <= cleido.R + 10)) &&
+                        ((cleido.G - 10 <= c.G) && (c.G <= cleido.G + 10)) &&
+                        ((cleido.B - 10 <= c.B) && (c.B <= cleido.B + 10)))
+                    {
+                        bmpxy1.SetPixel(i, j, Color.Yellow);
+                    }
+                    else
+                    {
+                        bmpxy1.SetPixel(i, j, Color.FromArgb(c.R, c.G, c.B));
+                    }
+
+                }
+            pictureBox1.Image = bmpxy1;
+
+            //-----------PARA LA SEGUNDA FILA 2------------------
+            int punto_x2 = 0, punto_y2 = 0;
+
+            punto_x2 = Convert.ToInt32(dataGridView1.Rows[1].Cells[0].Value);
+            punto_y2 = Convert.ToInt32(dataGridView1.Rows[1].Cells[1].Value);
+
+            cleido = bmp.GetPixel(punto_x2, punto_y2);
+            Bitmap bmp_x2_y2 = new Bitmap(pictureBox1.Image);
+            Bitmap bmpxy2 = new Bitmap(bmp_x2_y2.Width, bmp_x2_y2.Height);
+            //Color c = new Color();
+            for (int i = 0; i < bmpxy2.Width; i++)
+                for (int j = 0; j < bmpxy2.Height; j++)
+                {
+                    c = bmp_x2_y2.GetPixel(i, j);
+                    if (((cleido.R - 10 <= c.R) && (c.R <= cleido.R + 10)) &&
+                        ((cleido.G - 10 <= c.G) && (c.G <= cleido.G + 10)) &&
+                        ((cleido.B - 10 <= c.B) && (c.B <= cleido.B + 10)))
+                    {
+                        bmpxy2.SetPixel(i, j, Color.Blue);
+                    }
+                    else
+                    {
+                        bmpxy2.SetPixel(i, j, Color.FromArgb(c.R, c.G, c.B));
+                    }
+
+                }
+            pictureBox1.Image = bmpxy2;
+
+            //-----------PARA LA TERCERA FILA 3------------------
+            int punto_x3 = 0, punto_y3 = 0;
+
+            punto_x3 = Convert.ToInt32(dataGridView1.Rows[2].Cells[0].Value);
+            punto_y3 = Convert.ToInt32(dataGridView1.Rows[2].Cells[1].Value);
+
+            cleido = bmp.GetPixel(punto_x3, punto_y3);
+            Bitmap bmp_x3_y3 = new Bitmap(pictureBox1.Image);
+            Bitmap bmpxy3 = new Bitmap(bmp_x3_y3.Width, bmp_x3_y3.Height);
+            //Color c = new Color();
+            for (int i = 0; i < bmpxy3.Width; i++)
+                for (int j = 0; j < bmpxy3.Height; j++)
+                {
+                    c = bmp_x3_y3.GetPixel(i, j);
+                    if (((cleido.R - 10 <= c.R) && (c.R <= cleido.R + 10)) &&
+                        ((cleido.G - 10 <= c.G) && (c.G <= cleido.G + 10)) &&
+                        ((cleido.B - 10 <= c.B) && (c.B <= cleido.B + 10)))
+                    {
+                        bmpxy3.SetPixel(i, j, Color.Cyan);
+                    }
+                    else
+                    {
+                        bmpxy3.SetPixel(i, j, Color.FromArgb(c.R, c.G, c.B));
+                    }
+
+                }
+            pictureBox1.Image = bmpxy3;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Todos|*.*|Archivos JPEG|*.jpg|Archivos GIF|*.gif";
+            openFileDialog1.FileName = "";
+            openFileDialog1.ShowDialog();
+            bmp = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = bmp;
+
+          
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
